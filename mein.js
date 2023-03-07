e6310f48ba0eeb69703d6b9f9a8dc82490975399
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-03-07 16:09:23 +0500

    Update mein.js

--- mein.js
+++ mein.js
@@ -1,1 +1,24 @@
+let score = 0;
 
+function generateNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+function startGame() {
+  const randomNumber = generateNumber();
+  const userGuess = parseInt(window.prompt('Guess a number between 1 and 10'));
+  
+  if (userGuess === randomNumber) {
+    score += 5;
+    window.alert(`Congratulations! You guessed the number! Your score is ${score}.`);
+    const playAgain = window.confirm('Do you want to play again?');
+    if (playAgain) {
+      startGame();
+    }
+  } else {
+    window.alert(`Sorry, the number was ${randomNumber}. Try again.`);
+    startGame();
+  }
+}
+
+startGame();
